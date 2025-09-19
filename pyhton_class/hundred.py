89631b74b8ed35dcb35ca8fccd8191842b9ac443
jeongho0101 <eomj4039@gmail.com>
2025-09-19 18:53:32 +0900

    클래스2. 300제 260까지

---
+++ pyhton_class/hundred.py
@@ -0,0 +1,67 @@
+#251
+
+#252
+class human:
+    pass
+#253
+class human:
+    pass
+areum = human()
+#254
+class human:
+    def __init__(self):
+        print("응애응애")
+
+areum = human()
+
+#255
+class human:
+    def __init__(self,이름,나이,성별):
+        self.이름 = 이름
+        self.나이 = 나이
+        self.성별 = 성별
+
+areum = human("아름",25,"여자")
+
+#256
+class human:
+    def __init__(self,이름,나이,성별):
+        self.이름 = 이름
+        self.나이 = 나이
+        self.성별 = 성별
+    
+areum = human("아름",25,"여자")
+print(areum.이름)
+
+#257
+class human:
+    def __init__(self,이름,나이,성별):
+        self.이름 = 이름
+        self.나이 = 나이
+        self.성별 = 성별
+    
+    def who(self):
+        print(f"이름:{self.이름},나이:{self.나이},성별:{self.성별}")
+
+areum = human("아름",25,"여자")
+areum.who()
+
+#258
+class human:
+    def __init__(self,이름,나이,성별):
+        self.이름 = 이름
+        self.나이 = 나이
+        self.성별 = 성별
+    
+    def who(self):
+        print(f"이름:{self.이름},나이:{self.나이},성별:{self.성별}")
+
+    def setinfo(self,이름,나이,성별):
+        self.이름 = 이름
+        self.나이 = 나이
+        self.성별 = 성별
+
+#259
+11
+#260
+1111
