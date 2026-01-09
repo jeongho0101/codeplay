857ffae1818fcf2b1499f7c17f808c6b8565ed8b
jeongho0101 <eomj4039@gmail.com>
2026-01-09 14:32:29 +0900

    이벤트 완성 1/9

--- escape_game/event.py
+++ escape_game/event.py
@@ -17,6 +17,7 @@ Random = 0
 mhp = 100
 ehp = 0
 answer = ""
+keys = 2
 
 def gf(weapon):
     typing_Ani("할아버지: 용사여, 잠깐 얘기좀 하겠나.", 0.1)
@@ -133,13 +134,83 @@ def shop(weapon,mhp,answer):
         else:
             typing_Ani("점원: 그런 물건은 없습니다.", 0.1)
 
-def key_room():
+def key_room(keys,answer):
     Random = random.randint(1,2)
+    typing_Ani("상자가 2개 있다!",0.05)
+    typing_Ani("2개의 상자중 1개만 열수있다.",0.05)
+    while answer != 1 or answer !=2:
+        answer = input("어떤 상자를 여시겠습니까?(1 또는 2)->")
+        if int(answer) == Random:
+            typing_Ani("열쇠 2개를 얻었다!",0.05)
+            keys = 2
+            return keys
+        elif answer != Random:
+            typing_Ani("열쇠 1개를 얻었다!",0.05)
+            keys = 1
+            return keys
+        else:
+            typing_Ani("그런 상자는 없다.", 0.1)
+    
+def treasure1(weapon,answer):
+    typing_Ani("보물상자를 발견했다!",0.05)
+    typing_Ani("하지만 문제를 풀어야 가져갈수있다.",0.05)
+    typing_Ani("1번 문제.",0.05)
+    answer = input("마타톤 주행거리에서 파이 소수점 3번째 자리까지를 뺀 값은? ->")
+    if answer == "39.054":
+        typing_Ani("정답!!",0.05)
+        typing_Ani("하지만 두번째 문제가 남아있다.",0.05)
+        typing_Ani("2번 문제.",0.05)
+    elif answer != "39.054":
+        typing_Ani("틀렸다!!",0.05)
+        typing_Ani("상자가 불타 재가되었다",0.05)
+        typing_Ani("재가된 상자를 얻었다.",0.05)
+        return weapon
+    answer = input("미국의 지역이 아닌 것은?\n(1.오리건 2.조지아 3.펜실베니아 4.웰링턴 5.와이오밍) ->")
+    if answer == "4" or answer == "웰링턴":
+        typing_Ani("정답!!.",0.05)
+        typing_Ani("보물상자에서 전설의 용잡이칼(공격력 500)을 얻었다!",0.05)
+        typing_Ani("주의! 공격력이 높은 대신 공격 선택지가 4개로 늘어난다.",0.05)
+        weapon = 500
+        return weapon
+    elif answer != "4" or answer != "웰링턴":
+        typing_Ani("틀렸다!!",0.05)
+        typing_Ani("상자가 불타 재가되었다",0.05)
+        typing_Ani("재가된 상자를 얻었다.",0.05)
+        return weapon
+    
+def treasure2(mhp,answer):
+    typing_Ani("보물상자를 발견했다!",0.05)
+    typing_Ani("하지만 철창 뒤에 있다.",0.05)
+    typing_Ani("열쇠로 문을 열어야 한다.",0.05)
+    if keys == 2:
+        typing_Ani("남은 열쇠로 문을 열었다",0.05)
+        typing_Ani("하지만 문제를 풀어야 가져갈수있다.",0.05)
+        typing_Ani("1번 문제.",0.05)
+        answer = input("마블 케릭터가 아닌것은\n(1.아이언맨 2.블랙팬서 3.샤잠 4.스타로드 5.로켓 ->")
+        if answer == "3" or answer == "샤잠":
+            typing_Ani("정답!!",0.05)
+            typing_Ani("하지만 두번째 문제가 남아있다.",0.05)
+            typing_Ani("2번 문제.",0.05) 
+            answer = input("아시아에 있는 나라가 아닌것은?\n(1.한국 2.수리남 3.싱가포르 4.몰디브 5.사이프러스) ->")
+            if answer == "2" or answer == "수리남":
+                typing_Ani("정답!!.",0.05)
+                typing_Ani("보물상자에서 전설의 갑옷(방어력 300)을 얻었다!",0.05)
+                mhp = 300
+                return mhp
+            elif answer != "2" or answer != "수리남":
+                typing_Ani("틀렸다!!",0.05)
+                typing_Ani("상자가 불타 재가되었다",0.05)
+                typing_Ani("재가된 상자를 얻었다.",0.05)
+                return mhp
+        elif answer != "3" or answer != "샤잠":
+            typing_Ani("틀렸다!!",0.05)
+            typing_Ani("상자가 불타 재가되었다",0.05)
+            typing_Ani("재가된 상자를 얻었다.",0.05)
+            return mhp
+    elif keys == 1:
+        typing_Ani("열쇠가 없어 문을 열지 못했다",0.05)
+        return mhp
+    
 
-def treasure():
-    '''
-    1.상식퀴즈
-    2.산수문제
-    '''
-#gf(weapon)
-shop(weapon, mhp, answer)
+
+treasure2(keys,answer)
